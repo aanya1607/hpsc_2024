c707f3c1feca8886feeded0075aaa95e17c8e8b3
Eshika Choudhary <t24128@students.iitmandi.ac.in>
2025-02-05 03:59:53 +0530

    jupyter notebook on plots

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,14 +1,22 @@
-def sqrt2(x):
+def sqrt2(x,debug=False):
+    from numpy import nan
+    if x==0:
+       return 0;
+    elif x<0:
+       return nan
     s=1.0
     kmax=100
     tol=1.0e-14
     for k in range(kmax):
-        print(f"at insertion {k} the value of s={s:20.15f}")
+        if debug:
+             print(f"at insertion {k} the value of s={s:20.15f}")
         s0=s
         s=0.5*(s+(x/s))
         delta_s=s-s0
-        if(abs(delta_s)/x<tol):
+        if(abs(delta_s/x)<tol):
           break
-    print(f"Finally, the value of s={s:20.15f}")
+    if debug:
+        print(f"Finally, the value of s={s:20.15f}")
+    return s
         
 
